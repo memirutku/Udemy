95d31214fda4b0766ed801df7e4cf2e5ad8fe477
memirutku <72553236+memirutku@users.noreply.github.com>
2025-02-07 21:42:56 +0300

    13.Gün

--- 100 Günlük Programlama/PyCharm/example.py
+++ 100 Günlük Programlama/PyCharm/example.py
@@ -1,1 +1,19 @@
 print("Hello World")
+
+#python konsolu aslında bildiğimiz python sadece konsol üzerinde
+
+# terminal
+
+#clear : ekranı temizler
+
+#ifconfig : pc'nin yerel ip ve mac adreslerini verir
+
+#pwd: (print working directory) bize şu anda bulunduğumuz konumu gösterir.
+
+#ls: (list) bulunduğumuz konumun içinde bulunan dosyaları ve klosörleri gösterir.
+
+#cd: (change directory) cd yazıp sonra gideceğimiz konumu yazarsak oraya gideriz. Ex: cd Desktop
+    #cd .. ise bizi bir klasor geri götürürür
+
+# python:bulunduğumuz klasörde python dosyası varsa python(boşluk)main.py(ya da python dosyasının adı ne ise) yazarsak dosyayı çalıştırabiliriz.
+
