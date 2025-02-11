63c7a1ae05cc945253774a70b8fd97fb7425c50d
memirutku <72553236+memirutku@users.noreply.github.com>
2025-02-12 00:38:24 +0300

    14.Gün

--- 100 Günlük Programlama/EasyProjects/pdfConverter/main.py
+++ 100 Günlük Programlama/EasyProjects/pdfConverter/main.py
@@ -1,8 +1,13 @@
 from pdf2docx import Converter
+# pdf i word e çeviriyor.
 
-pdf_file = "sample.pdf"
-docx_file = "sample.docx"
+pdf_path = "sample.pdf"
+docx_file = "sample.docx" #docx word dosyasının uzantısı
 
-cv = Converter(pdf_file)
-cv.convert(docx_file)
+cv = Converter(pdf_file=pdf_path) # pdf_file converter methoduna girilmesi gereken değer adı 'pdf_file=pdf_path' bu şekilde belirterek
+#pdf_file değerinin pdf path olduğunu belirtiyoruz okunması için pozitif etkisi var. Converter(pdf_path) yazsak da sıkıntı olmayacaktı.
+# ama diyelim birden fazla değer girmemiz lazım. methodda değer sırası olarak değerleri sırayla girmemiz lazım
+# ama Converter(pdf_file=pdf_path) şeklinde yaparsak birden fazla değer girsek bile (birbirinden farklı değerlere atanmak koşuluyla)
+# belirtiğimiz sürece sıraya önem vermemize gerek yok.
+cv.convert(docx_filename=docx_file)
 cv.close()
--- 100 Günlük Programlama/EasyProjects/qrCodeGenerator/main.py
+++ 100 Günlük Programlama/EasyProjects/qrCodeGenerator/main.py
@@ -3,4 +3,5 @@ import pyqrcode
 url = input("enter url to generate qr code: ")
 
 qr_code = pyqrcode.create(url)
-qr_code.svg('qrcode.svg',scale=5)
+qr_code.svg('qrcode.svg',scale=5) # farklı farklı uzantı vererek oluşturabiliriz.
+# . dan sonra tab basarak deneme yanılma ile de yapabiliriz.
