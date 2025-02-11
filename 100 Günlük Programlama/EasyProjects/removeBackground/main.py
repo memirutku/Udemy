1964496f23724e7c7fb031f5775e50857e7eba14
memirutku <72553236+memirutku@users.noreply.github.com>
2025-02-11 23:48:30 +0300

    14.Gün

--- 100 Günlük Programlama/EasyProjects/removeBackground/main.py
+++ 100 Günlük Programlama/EasyProjects/removeBackground/main.py
@@ -1,10 +1,11 @@
 from rembg import remove
+#cv2.cvtColor ye de bakılabilir özellikle görsel işleme konusunda
+input_path = "image.jpeg" #başka yerde olursa tam konumunu yazmak lazım
+output_path = "output.png" #dökümantasyona baktığımızda çıktıyı png olarak verin demiş
+#uzantısını png yaptık o yüzden
 
-input_path = "image.jpeg"
-output_path = "output.png"
-
-with open(input_path, 'rb') as i:
-    with open(output_path, 'wb') as o:
+with open(input_path, 'rb') as i: # 'rb' read binary olarak açıyor demek 0,1 gibi şeyleri okuyarak dosya olarak açıyor
+    with open(output_path, 'wb') as o: # 'wb' write binary olarak yazıyor demek 0,1 gibi şeyleri okuyarak dosya olarak yazıyor
         input = i.read()
-        output = remove(input)
+        output = remove(input) # rembg nin methodu
         o.write(output)
--- 100 Günlük Programlama/EasyProjects/youtubeDownloader/main.py
+++ 100 Günlük Programlama/EasyProjects/youtubeDownloader/main.py
@@ -2,6 +2,9 @@ import pytube
 
 url = input("enter video url: ")
 
-path = ""
+path = "" # ("") dosyayı nereye çalıştırıyorsam oraya kaydeder.
 
 pytube.YouTube(url).streams.get_highest_resolution().download(path)
+
+# . lardan sonra tab a basarsan method isimleriyle ekstra şeyler ekleyebilirsin
+# dökümantasyonu okumak önemli
