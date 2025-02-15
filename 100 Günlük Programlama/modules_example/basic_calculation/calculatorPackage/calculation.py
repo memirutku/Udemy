976151b05e7a08d6b837c38c23bafae3587b4f26
memirutku <72553236+memirutku@users.noreply.github.com>
2025-02-15 23:41:17 +0300

    Update calculation.py

--- 100 Günlük Programlama/modules_example/basic_calculation/calculatorPackage/calculation.py
+++ 100 Günlük Programlama/modules_example/basic_calculation/calculatorPackage/calculation.py
@@ -15,3 +15,8 @@ def multiply(x: int,y: int ):
 
 def divide(x: int,y: int ):
     return x/y
+
+# buradan işlemi geri döndürmek lazım
+# x - y çalıştıktan sonra **args ile yapmak lazım
+# ve işlem bittiğinde hep tekrar tekrar işlem yapacak
+# bunların hepsi örneklerde ve hem çalışmışda oluruz şimdi hastayım sonra devam edicem
