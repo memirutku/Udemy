df566607846224edda312c4cceed25c1ab9d6192
memirutku <72553236+memirutku@users.noreply.github.com>
2025-02-15 23:32:54 +0300

    14.Gün

--- 100 Günlük Programlama/modules_example/basic_calculation/calculatorPackage/calculation.py
+++ 100 Günlük Programlama/modules_example/basic_calculation/calculatorPackage/calculation.py
@@ -1,4 +1,9 @@
 
+def __init__(self,x,y):
+    self.x=x
+    self.y=y
+
+
 def plus(x: int,y: int ):
  return x+y
 
--- 100 Günlük Programlama/modules_example/basic_calculation/main.py
+++ 100 Günlük Programlama/modules_example/basic_calculation/main.py
@@ -4,14 +4,13 @@ import calculatorPackage as calc
 
 input = input("1. Toplama işlemi yapmak istiyorsan 1'e tıklayın \n2. Çıkarma işlemi yapmak istiyorsan 2'e tıklayın \n3. Çarpma işlemi yapmak istiyorsan 3'e tıklayın\n4. Bölme işlemi yapmak istiyorsan 4'e tıklayın" )
 
-
-match ():
-
-    case input = "1":
-        calc.add(1)
-    case input = "2":
-        calc.minus(2)
-    case input = "3":
-        calc.multiply(3)
-    case input = "4":
-        calc.divide(3)
+def islem(input):
+    match input:
+        case "1":
+            return calc.add(1,2)
+        case "2":
+            return calc.minus(2,3)
+        case "3":
+            return calc.multiply(3,4)
+        case "4":
+            return calc.divide(3,6)
