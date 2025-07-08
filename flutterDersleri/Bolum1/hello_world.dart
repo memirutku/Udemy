1235eefc388fe1b98cc35ae9b34365fdaf5fc943
memirutku <mustafa.emir@marun.edu.tr>
2025-07-08 15:23:20 +0300

    Flutter

---
+++ flutterDersleri/Bolum1/hello_world.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  print("Hello, World!"); // noktalı virgül önemli
+  print("Merhaba, Dünya!");
+  print(3 + 5); // parantez içinde matematik işlemi yapabiliyor.
+  print("mustafa' nın çalışması");
+  print("mustafa\"nın çalışması"); // çift tırnak içinde çift tırnak kullanımı
+  print('mustafa\'nın çalışması'); // tek tırnak içinde tek tırnak kullanımı
+}
