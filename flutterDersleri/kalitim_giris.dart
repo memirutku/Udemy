a9d6b7940f6ad1ab288eb1c529a808ca986a8d8f
memirutku <mustafa.emir@marun.edu.tr>
2025-07-24 13:34:58 +0300

    Add inheritance example with User classes in Dart
    Introduced a class hierarchy demonstrating inheritance in Dart, including User, AdminUser, NormalUser, and SadeceOkuyabilenNormalUser. Added examples of method overriding and class extension to illustrate object-oriented principles.

--- flutterDersleri/kalitim_giris.dart
+++ flutterDersleri/kalitim_giris.dart
@@ -11,5 +11,98 @@ Javadaki çoklu kalıtım gibi bir şey yok. Tekli kalıtım var.
 Dart'ta çoklu kalıtım yok ama
 //mixin ile çoklu kalıtım yapabiliriz.(önemli olabilir))
 Dart'ta kalıtım, extends anahtar kelimesi ile yapılır. Bir sınıf, başka bir sınıfı extends ederek o sınıfın özelliklerini ve metotlarını devralır.
+/**
+ * KALITIM INHERITANCE : İki adet sınıf düşünelim. Birinin adı Ebeveyn diğerinin adı Çocuk olsun. 
+ * Ebeveyn sınıfının özelliklerini anne+baba’nın işlevleri oluşturmaktadır. 
+ * Çocuk ise bazı özellikler bakımından anne babaya benzerlik göstermektedir. (saç rengi, göz rengi) 
+ * Bunların yanından çocukta anne ve babasında olmayan bazı özelliklerde yer alabilir.  ( Anne baba kısa boylu iken çocuğun uzun olması). 
+ * Ebeveyn gibi üst sınıflara süper sınıflar denir.  Bunlardan türetilen alt sınıflara alt sınıflar denir. 
 
+Dart da kalıtım bir sınıfın kendine ait özellikleri başka bir sınıfa aynen aktarması ya da bazı özellikleri diğer sınıflara izin vermesidir.
+
+Extends anahtar kelimesi: bir sınıfa ait özelliklere başka bir sınava miras almak için kullanılır.
+
+Her alt sınıf doğrudan bir tane süper sınıfa sahip olabilir. 
+Her alt sınıf süper sınıfın özelliklerini taşır.  
+Süper sınıftan alınan bazı özellikler değiştirilebilir. Aynen kullanılmak zorunda değildir. 
+Kalıtımın basamakları çok uzun olmamalıdır.  Bu kötü tasarıma neden olur.
+
+Kalıtımın Faydaları
+Daha önceden yazılmış kod parçacıkları tekrar tekrar yazılmadan başka sınıflarda kullanılabilir.  
+if, else, switch gibi kontrol ifadelerinin oolduğu sınıflar kötü tasarım örnekleridir. Bunun yerine kalıtım tercih edilmelidir.
+
+Method overriding : üst sınıftaki değişken veya methodların alt sınıf tarafından değiştirilmesidir.
+ */
 */
+
+//İki yapı için ortak değerler var bunları kalıtım ile yapabiliriz.
+/*
+
+void main(List<String> args) {}
+
+
+class NormalUser {
+  String email = " ";
+  String password = " ";
+  void girisYap() {print("Normal user giriş yaptı.");
+  }
+  void davetEt() {
+      print("Kullanıcı davet edildi.");
+    }
+}
+
+
+class AdminUser {
+String email = " ";
+  String password = " ";
+  void girisYap() {
+    print("Admin user giriş yaptı.");
+  }
+void toplamKullaniciSayisiniGoster() {
+    print("Toplam kullanıcı sayısı: 100");
+  }
+}
+*/
+//İki yapı için ortak değerler var bunları kalıtım ile yapabiliriz.
+
+void main(List<String> args) {
+  AdminUser admin = AdminUser();
+  admin.girisYap(); // bu User sınıfından geliyor.
+  SadeceOkuyabilenNormalUser user = SadeceOkuyabilenNormalUser();
+  user.girisYap(); // bu User sınıfından geliyor.
+}
+
+class User {
+  String email = " ";
+  String password = " ";
+
+  void girisYap() {
+    print("Main User giriş yaptı.");
+  }
+}
+
+class AdminUser extends User {
+  //böylelikle userin içinde bulunanları da burada kullanabiliriz.
+  void toplamKullaniciSayisiniGoster() {
+    print("Toplam kullanıcı sayısı: 100");
+  }
+}
+
+class NormalUser extends User {
+  void davetEt() {
+    print("Kullanıcı davet edildi.");
+  }
+}
+
+class SadeceOkuyabilenNormalUser extends NormalUser {
+  //bu hem NormalUser hem de User sınıfından özellikleri alır.
+  void adiniSoyle() {
+    print("Ben sadece okuyabilen normal bir kullanıcıyım.");
+  }
+  @override
+  void girisYap(){
+      print("Sadece okuyabilen normal kullanıcı giriş yaptı.");
+    } //ovver ride ile üst sınıftaki metodu kendine göre değiştirebiliyoruz.
+
+    //girisyap metodunu yine de Userden aldığı için perfomanslı olur.
+}
